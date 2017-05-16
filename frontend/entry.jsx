f088857eb955339b88f86e8579284ba4e131d2e3
Tu Ngo <tungo8810@gmail.com>
2017-05-16 12:14:49 -0700

    Update entry.jsx and fix bugs

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import Root from './components/root';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(
-    <h1>Instagames App</h1>,
+    <Root store={store} />,
     root
   );
 });
