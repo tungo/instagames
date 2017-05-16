b5cf6e3dd8cc8bb3f9a2ac92e1d35661772d94f3
Tu Ngo <tungo8810@gmail.com>
2017-05-16 08:50:35 -0700

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ActiveRecord::Base
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by_username(username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
