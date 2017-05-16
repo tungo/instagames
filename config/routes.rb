0be4edb252a72cba6c90ad011534bf0a0170ec58
Tu Ngo <tungo8810@gmail.com>
2017-05-16 09:09:32 -0700

    Add Routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
+
+  namespace :api, default: {format: :json} do
+    resources :users, only: [:create]
+    resource :sessions, only: [:create, :destroy]
+  end
 end
