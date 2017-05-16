bc4eea4f373f1b29923c8bf3acb8c84ecdeacf3b
Tu Ngo <tungo8810@gmail.com>
2017-05-16 15:29:56 -0700

    Implement AuthRoute SessionForm into App

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { AuthRoute} from '../util/route_util.jsx';
+
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
     <h1>Instagames</h1>
+
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
   </div>
 );
 
