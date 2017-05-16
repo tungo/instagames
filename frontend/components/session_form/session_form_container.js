078defefb06ccc2656b44cff774a8cb5d30638fa
Tu Ngo <tungo8810@gmail.com>
2017-05-16 15:19:16 -0700

    Add AuthRoute and ProtectedRoute in route_util.jsx

--- frontend/components/session_form/session_form_container.js
+++ frontend/components/session_form/session_form_container.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux';
 import { login, signup } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
-const mapStateToProps = ({ session }) => ({
-  loggedIn: Boolean(session.currentUser),
-  errors: session.errors,
+const mapStateToProps = (state) => ({
+  errors: state.session.errors,
 });
 
 const mapDispatchToProps = (dispatch, { location }) => {
