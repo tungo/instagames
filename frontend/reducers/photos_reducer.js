028238bb00ecd2b74ac739c8f25d21c058b2b5e6
Tu Ngo <tungo8810@gmail.com>
2017-05-17 15:40:01 -0700

    Add PhotosReducer

---
+++ frontend/reducers/photos_reducer.js
@@ -0,0 +1,24 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_PHOTOS,
+  RECEIVE_PHOTO
+} from '../actions/photo_action';
+
+const PhotosReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_PHOTOS:
+      return merge({}, state, action.photos);
+
+    case RECEIVE_PHOTO:
+      const { photo } = action;
+      return merge({}, state, {[photo.id]: photo})
+
+    default:
+      return state;
+  }
+};
+
+export default PhotosReducer;
