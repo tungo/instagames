ac143f792927b1a7fed6be514a16a38d003c3b07
Tu Ngo <tungo8810@gmail.com>
2017-05-17 08:03:54 -0700

    Update Production ready settings

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  // const createLogger = require('redux-logger');
+  // middlewares.push(createLogger());
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(...middlewares))
   )
 );
 
