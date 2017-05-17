fba8577985e81734a2ee9ad7c50fb2a703e149a0
Tu Ngo <tungo8810@gmail.com>
2017-05-17 15:33:33 -0700

    Add Photo Actions

---
+++ frontend/actions/photo_actions.js
@@ -0,0 +1,28 @@
+import * as PhotoAPIUtil from '../util/photo_api_util';
+
+export const RECEIVE_PHOTOS = 'RECEIVE_PHOTOS';
+export const RECEIVE_PHOTO = 'RECEIVE_PHOTO';
+
+
+export const receivePhotos = (photos) => ({
+  type: RECEIVE_PHOTOS,
+  photos
+});
+
+export const receivePhoto = (photo) => ({
+  type: RECEIVE_PHOTO,
+  photo
+});
+
+
+export const feedPhotos = () => (dispatch) => (
+  PhotoAPIUtil.feedPhotos()
+    .then((photos) => dispatch(receivePhotos(photos)))
+);
+
+export const createPhoto = (photo) => (dispatch) => (
+  PhotoAPIUtil.createPhoto(photo)
+    .then((rspPhoto) => dispatch(receivePhoto(rspPhoto)))
+    .fail((err) => console.log(err))
+);
+
