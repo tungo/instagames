bce055890266911168679a7dc47b5564a9773f6f
Tu Ngo <tungo8810@gmail.com>
2017-05-16 17:14:06 -0700

    Add Page Component

---
+++ frontend/components/page/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import NavContainer from './nav/nav_container';
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavContainer />
+
+      </div>
+    );
+  }
+}
+
+export default Page;
