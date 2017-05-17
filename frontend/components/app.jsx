793ab7f4dc56c0f7bf3c0a9c009402f9dcd635db
Tu Ngo <tungo8810@gmail.com>
2017-05-16 17:15:31 -0700

    Implement Page into App

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { AuthRoute} from '../util/route_util.jsx';
+import { AuthRoute, ProtectedRoute } from '../util/route_util.jsx';
 
 import SessionFormContainer from './session_form/session_form_container';
+import Page from './page/page';
 
 const App = () => (
   <div>
-    <h1>Instagames</h1>
-
     <Switch>
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/signup" component={SessionFormContainer} />
+
+      <ProtectedRoute path="/" component={Page} />
     </Switch>
   </div>
 );
