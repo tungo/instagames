523d8f9dc2d39be9defed485b0dcd51b597de0a1
Tu Ngo <tungo8810@gmail.com>
2017-05-17 15:23:01 -0700

    Add photo_api_util.js

---
+++ frontend/util/photo_api_util.js
@@ -0,0 +1,43 @@
+export const feedPhotos = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/photos'
+  })
+);
+
+export const requestPhoto = (id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/photos/${id}`
+  })
+);
+
+export const createPhoto = (photo) => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/photos',
+    data: {photo}
+  })
+);
+
+export const updatePhoto = (photo) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/photos/${photo}`,
+    data: {photo}
+  })
+);
+
+export const deletePhoto = (id) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/photos/${id}`,
+  })
+);
+
+export const requestUserPhotos = (user_id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/users/${user_id}/photos`
+  })
+);
