1f38de6c50bd4908ad19825af0230fe974ca7f87
Tu Ngo <tungo8810@gmail.com>
2017-05-17 11:07:10 -0700

    Add Photos routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do
+      get '/photos', to: 'photos#user', as: 'photo'
+    end
     resource :session, only: [:create, :destroy]
+
+    resources :photos, only: [:index, :create, :show, :update, :destroy]
   end
 end
