d7ac72639ca8eee7262c31491573a2c50539fcab
Tu Ngo <tungo8810@gmail.com>
2017-05-19 09:55:54 -0700

    Add AvatarModal

---
+++ frontend/components/page/profile/avatar_modal.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+import AvatarFormContainer from './avatar_form_container';
+
+const style = {
+  overlay: {
+    // display: 'flex', // make content stay at center
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 10,
+
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  content: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    margin: '100px auto',
+    width: '500px',
+    padding: 0,
+    zIndex: 11,
+
+    border: 'none',
+    borderRadius: 'none'
+  }
+};
+
+class AvatarModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalOpen: false
+    };
+
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  closeModal(e) {
+    this.setState({modalOpen: false});
+  }
+
+  openModal(e) {
+    this.setState({modalOpen: true});
+  }
+
+  render() {
+    return (
+      <figure className="avatar-modal">
+        <img
+          src={this.props.url}
+          alt={this.props.username}
+          onClick={this.openModal}
+        />
+
+        <Modal
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.closeModal}
+          style={style}
+          contentLabel="AvatarModal"
+        >
+          <AvatarFormContainer closeModal={this.closeModal} />
+        </Modal>
+      </figure>
+    );
+  }
+}
+
+export default AvatarModal;
