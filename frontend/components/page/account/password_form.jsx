c9d3fca51e2ec15bca6f65ef29ba63702ec7f91c
Tu Ngo <tungo8810@gmail.com>
2017-05-19 14:44:57 -0700

    Add PasswordForm Components

---
+++ frontend/components/page/account/password_form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EditForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <article>
+        password
+      </article>
+    );
+  }
+}
+
+export default EditForm;
---
+++ frontend/components/page/account/password_form_container.js
@@ -0,0 +1,5 @@
+import { connect } from 'react-redux';
+
+import PasswordForm from './password_form';
+
+export default connect(null)(PasswordForm);
