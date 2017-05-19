600a30053a5e3b47f3980e93338ff2e2ff49818c
Tu Ngo <tungo8810@gmail.com>
2017-05-19 08:57:21 -0700

    Add PhotoDetail

---
+++ frontend/components/page/profile/photo_detail.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class PhotoDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { photo } = this.props;
+
+    let caption = '';
+    if (photo.caption) {
+      caption = <div className="caption">
+        <span>{photo.username}</span>
+        {photo.caption}
+      </div>;
+    }
+
+    return (
+      <article className="photo-detail">
+        <figure className="left">
+          <img src={photo.url} alt={`photo-${photo.caption}`} />
+        </figure>
+
+        <section className="right">
+          <header>
+            <img src={photo.avatar} alt={`photo-${photo.caption}`} />
+            {photo.username}
+          </header>
+
+          <aside className="info">
+            <div className="uploaded-at">
+              {photo.uploaded_at}
+            </div>
+
+            {caption}
+
+          </aside>
+        </section>
+      </article>
+    );
+  }
+}
+
+export default PhotoDetail;
