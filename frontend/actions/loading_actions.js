f589d51772847f37811673abcb3ecd0b85e71f13
Tu Ngo <tungo8810@gmail.com>
2017-05-20 19:11:02 -0700

    Add Loading style and fix bugs

--- frontend/actions/loading_actions.js
+++ frontend/actions/loading_actions.js
@@ -1,11 +1,11 @@
 export const START_LOADING = 'START_LOADING';
-export const END_LOADING = 'END_LOADING';
+export const STOP_LOADING = 'STOP_LOADING';
 
 
 export const startLoading = () => ({
   type: START_LOADING
 });
 
-export const endLoading = () => ({
-  type: END_LOADING
+export const stopLoading = () => ({
+  type: STOP_LOADING
 });
--- frontend/reducers/loading_reducer.js
+++ frontend/reducers/loading_reducer.js
@@ -1,10 +1,26 @@
-import { START_LOADING, END_LOADING } from '../actions/loading_actions';
+import { START_LOADING, STOP_LOADING } from '../actions/loading_actions';
+import {
+  RECEIVE_PHOTO,
+  RECEIVE_PHOTOS,
+  RECEIVE_PHOTO_DETAIL
+} from '../actions/photo_actions';
+import {
+  RECEIVE_USER
+} from '../actions/user_actions';
 
 const LoadingReducer = (state = false, action) => {
+  Object.freeze(state);
+
   switch(action.type) {
     case START_LOADING:
       return true;
-    case END_LOADING:
+    case STOP_LOADING:
+      return false;
+
+    case RECEIVE_PHOTO:
+    case RECEIVE_PHOTOS:
+    case RECEIVE_PHOTO_DETAIL:
+    case RECEIVE_USER:
       return false;
 
     default:
