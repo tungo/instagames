48861419eb202105efef327753eac26d76a2d9e1
Tu Ngo <tungo8810@gmail.com>
2017-05-22 10:24:47 -0700

    Add FollowsController and views

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,32 @@
+class Api::FollowsController < ApplicationController
+  def following
+    @follows = Follow.where(follower_id: params[:user_id])
+    render :index
+  end
+
+  def followers
+    @follows = Follow.where(following_id: params[:user_id])
+    render :index
+  end
+
+  def create
+    @follow = Follow.new(following: params[:user_id])
+    @follow.follower_id = current_user.id
+
+    if @follow.save
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @follow = current_user.out_follows.find_by_following_id(params[:user_id])
+
+    if @follow.destroy
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
+  end
+end
