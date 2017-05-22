764ba31e19ea5cba121f0f87b14aa91aad39c42e
Tu Ngo <tungo8810@gmail.com>
2017-05-22 11:03:14 -0700

    Add Routes for Likes Comments and Follows

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,19 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create, :update] do
       get '/photos', to: 'photos#user', as: 'photo'
+
+      resources :follows, only: [:create]
+      delete '/follows', to: 'follows#destroy'
+      get '/following', to: 'follows#following', as: 'following'
+      get '/followers', to: 'follows#followers', as: 'followers'
     end
     resource :session, only: [:create, :destroy]
 
-    resources :photos, only: [:index, :create, :show, :update, :destroy]
+    resources :photos, only: [:index, :create, :show, :destroy] do
+      resources :likes, only: [:create]
+      delete '/likes', to: 'likes#destroy'
+    end
+
+    resources :comments, only: [:create, :destroy]
   end
 end
